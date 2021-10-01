6909d41b5acfc376b786eedf2e0768b70163eab8
LWabnic <l.wabnic@outlook.com>
2021-10-01 14:17:26 +0100

    Created RecordService Class , Fixed RecordsRepo import issue, need to create config and add mapper

--- src/main/java/com/ringside/repo/RecordRepo.java
+++ src/main/java/com/ringside/repo/RecordRepo.java
@@ -1,17 +1,13 @@
 package com.ringside.repo;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-
+import com.ringside.domain.Record;
 
 @Repository
 public interface RecordRepo extends JpaRepository<Record,Long> {
 
-//	@Query(value="SELECT * FROM person Where winner= ?1",nativeQuery = true)
-//	List<Record> findByName(String winner);
-	
-}
+//		@Query(value="SELECT * FROM person Where winner= ?1",nativeQuery = true)
+//		List<Record> findByName(String winner);
+		
+	}
+
